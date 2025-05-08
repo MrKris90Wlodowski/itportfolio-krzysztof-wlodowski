37a9d1513e3fd98da05aafa85cb59e6ca7f41d8e
Kris1990_PL <MRK1990@wp.pl>
2025-05-08 18:03:27 +0200

    Add logic for switching between sections using dataset

--- pracaZaliczeniowa.js
+++ pracaZaliczeniowa.js
@@ -5,15 +5,41 @@ const goldMenuMobile = document.getElementById("goldMenuMobile");
 const menuListMobile = document.getElementById("menuListMobile");
 
 function showHideMobileMenu () {
-    menuListMobile.classList.toggle("menuActiveElement");
-    normalMenuMobile.classList.toggle("menuActiveElement");
-    goldMenuMobile.classList.toggle('menuActiveElement');
+    menuListMobile.classList.toggle("activeElement");
+    normalMenuMobile.classList.toggle("activeElement");
+    goldMenuMobile.classList.toggle('activeElement');
 }
 
 normalMenuMobile.addEventListener("click", showHideMobileMenu )
 
 goldMenuMobile.addEventListener("click", showHideMobileMenu )
 
+// SHOW SECTION
+
+const activeNavListElement = document.querySelectorAll("nav li");
+
+activeNavListElement.forEach(element => {
+    element.addEventListener("click", () => {
+    activeNavListElement.forEach(navList => navList.classList.remove("activeListElement"));
+    const targetList = element.dataset.target;
+    const similarNavList = document.querySelectorAll(`[data-target="${targetList}"]`);
+    similarNavList.forEach(navList => navList.classList.add("activeListElement"));
+
+    const logoHeader = document.querySelectorAll("[data-section-header]");
+    logoHeader.forEach(logo => { 
+    logo.classList.add("hiddenElement");
+    if ( logo.dataset.sectionHeader === targetList) {
+        logo.classList.remove("hiddenElement")
+    }})
+    
+    const mainSections = document.querySelectorAll("[data-section]");
+    mainSections.forEach(section => {
+    section.classList.add("hiddenElement");
+    if (section.dataset.section === targetList) {
+        section.classList.remove("hiddenElement")
+    }})
+})
+})
 
 
 
