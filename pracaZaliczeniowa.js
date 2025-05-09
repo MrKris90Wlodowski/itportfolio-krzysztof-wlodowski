6687c7ba0ee9796a763caaf4a705a08a8aa88f60
Kris1990_PL <MRK1990@wp.pl>
2025-05-09 15:15:10 +0200

    Add contact button

--- pracaZaliczeniowa.js
+++ pracaZaliczeniowa.js
@@ -17,6 +17,8 @@ goldMenuMobile.addEventListener("click", showHideMobileMenu )
 // SHOW SECTION
 
 const activeNavListElement = document.querySelectorAll("nav li");
+const logoHeader = document.querySelectorAll("[data-section-header]");
+const mainSections = document.querySelectorAll("[data-section]");
 
 activeNavListElement.forEach(element => {
     element.addEventListener("click", () => {
@@ -25,14 +27,12 @@ activeNavListElement.forEach(element => {
     const similarNavList = document.querySelectorAll(`[data-target="${targetList}"]`);
     similarNavList.forEach(navList => navList.classList.add("activeListElement"));
 
-    const logoHeader = document.querySelectorAll("[data-section-header]");
     logoHeader.forEach(logo => { 
     logo.classList.add("hiddenElement");
     if ( logo.dataset.sectionHeader === targetList) {
         logo.classList.remove("hiddenElement")
     }})
     
-    const mainSections = document.querySelectorAll("[data-section]");
     mainSections.forEach(section => {
     section.classList.add("hiddenElement");
     if (section.dataset.section === targetList) {
@@ -41,5 +41,23 @@ activeNavListElement.forEach(element => {
 })
 })
 
+// CONTACT BUTTON 
+
+const contactButton = document.getElementById("contactButton");
+
+contactButton.addEventListener("click", () => {
+    activeNavListElement.forEach(navList => navList.classList.remove("activeListElement"));
+    const contactNavList = document.querySelectorAll('[data-target="contact"]');
+    contactNavList.forEach(contactList => contactList.classList.add("activeListElement"));
+
+logoHeader.forEach(logo => logo.classList.add("hiddenElement"));
+const logoMessages = document.querySelector('[data-section-header="contact"]');
+logoMessages.classList.remove("hiddenElement");
+
+mainSections.forEach(section => section.classList.add("hiddenElement"));
+const sectionContact = document.querySelectorAll('[data-section="contact"]');
+sectionContact.forEach(secContact => secContact.classList.remove("hiddenElement"))
+
+})
 
 
