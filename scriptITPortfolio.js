297b5813789aaa1ec13b939dc57b13124a7349e5
Kris1990_PL <MRK1990@wp.pl>
2025-05-27 20:22:58 +0200

    add main userData object

--- scriptITPortfolio.js
+++ scriptITPortfolio.js
@@ -103,6 +103,36 @@ function getCardTotalWidthtWithGap() {
 }
 // const totalCardWidth = getCardTotalWidthtWithGap();
 
+const userData = {
+  firstName: "Krzysztof",
+  lastName: "Włodowski",
+  info: {
+    introduce:
+      "Hello! I'm Krzysztof Włodowski, a person passionate about continuous growth and exploring new paths. My background is diverse,\n my approach to life is rooted in acquiring knowledge and achieving goals with full dedication.\n Feel free to discover more about my professional journey and interests!",
+    myBackground: 
+      "I have a diverse background that enriches both my professional career and personal growth.\n I am constantly striving to develop myself and make progressin every area of life I am involved in.\n My professional experience has taught me to be diligent incarrying out my responsibilities, take ownership of assignedtasks, and aim for the best possible final result.",
+    hobbies:
+      "In my free time, I enjoy developing my passions, especially physical activity and DIY projects.\n Both of these hobbies allow me to spend my time creatively and productively, while constantly growing and becoming better at what I do."
+  },
+  techSkill: [
+    { skill: "HTML", experience: 1 },
+    { skill: "CSS", experience: 1 },
+    { skill: "Java Script", experience: 1 },
+    { skill: "Git", experience: 1 },
+    { skill: "Figma", experience: 1 },
+    { skill: "Chrome", experience: 2 },
+    { skill: "VSCode", experience: 2 },
+    { skill: "GitHub", experience: 1 },
+  ],
+  cardsProjects: [
+    { project: "Calculator", technology: "HTML" },
+    { project: "Non-governmental organization", technology: "HTML,CSS" },
+    { project: "Calculator program", technology: "Java Script" },
+    { project: "Calculator", technology: "HTML" },
+    { project: "Non-governmental organization", technology: "HTML,CSS" },
+  ]
+}
+
 // LOGIC: mobile menu
 
 // FUNCTION: show and hide hamburger menu
