f51e596a39113e4ecca0ee9af10ea69327e76f5b
Kris1990_PL <MRK1990@wp.pl>
2025-10-05 19:20:07 +0200

    chore(userInfo): create individual information object

--- scriptITPortfolio.js
+++ scriptITPortfolio.js
@@ -1,3 +1,6 @@
+// IMPORT: object include user indo data
+import userInfo from "./userInfo.js"
+
 // ELEMENTS: mobile menu
 const normalMenuMobile = document.getElementById("normalMenuMobile");
 const goldMenuMobile = document.getElementById("goldMenuMobile");
@@ -89,60 +92,12 @@ function getCardTotalWidthtWithGap() {
   return 0;
 }
 
-// ELEMENTS: main user data object
-const userData = {
-  firstName: "Krzysztof",
-  lastName: "Włodowski",
-  info: {
-    introduce:
-      "Hello! I'm Krzysztof Włodowski, a person passionate about continuous growth and exploring new paths.\n My background is diverse, my approach to life is rooted in acquiring knowledge and achieving goals with full dedication.\n Feel free to discover more about my professional journey and interests!",
-    myBackground:
-      "I have a diverse background that enriches both my professional career and personal growth.\n I am constantly striving to develop myself and make progressin every area of life I am involved in.\n My professional experience has taught me to be diligent incarrying out my responsibilities, take ownership of assignedtasks, and aim for the best possible final result.",
-    hobbies:
-      "In my free time, I enjoy developing my passions, especially physical activity and DIY projects.\n Both of these hobbies allow me to spend my time creatively and productively, while constantly growing and becoming better at what I do.",
-  },
-  techSkill: [
-    { skill: "HTML", experience: 2 },
-    { skill: "CSS", experience: 1 },
-    { skill: "Java Script", experience: 1 },
-    { skill: "Git", experience: 1 },
-    { skill: "Figma", experience: 1 },
-    { skill: "Chrome", experience: 4 },
-    { skill: "VSCode", experience: 2 },
-    { skill: "GitHub", experience: 1 },
-  ],
-  cardsProjects: [
-    { project: "Calculator", technology: "HTML" },
-    { project: "Non-governmental organization", technology: "HTML,CSS" },
-    { project: "Calculator program", technology: "Java Script" },
-    { project: "Calculator", technology: "HTML,C++" },
-    { project: "Non-governmental organization", technology: "HTML,CSS" },
-  ],
-  messages: [
-    {
-      name: "Karol",
-      email: "karol@email.com",
-      message: "Hello, I've reviewed your impressive portfolio and am interested in disscussing a potential collaboration. Please call me at 712-218-123 to talk further."
-    },
-    {
-      name: "Ernest",
-      email: "ernest@email.com",
-      message: "Hello, Please call me at 351-152-555 to talk further."
-    },
-    {
-      name: "Jan",
-      email: "jan@email.com",
-      message: "Welcome Jan. You created really nice project"
-    }
-  ]
-};
-
 // DYNAMIC RENDER: first name last name and info 
-nameUser.textContent = (userData.firstName).toUpperCase();
-fullName.textContent = `${userData.firstName} ${userData.lastName}`.toUpperCase();
-introduce.textContent = userData.info.introduce;
-myBackground.textContent = userData.info.myBackground;
-hobbies.textContent = userData.info.hobbies;
+nameUser.textContent = (userInfo.firstName).toUpperCase();
+fullName.textContent = `${userInfo.firstName} ${userInfo.lastName}`.toUpperCase();
+introduce.textContent = userInfo.info.introduce;
+myBackground.textContent = userInfo.info.myBackground;
+hobbies.textContent = userInfo.info.hobbies;
 
 // LOGIC: mobile menu
 
@@ -762,7 +717,7 @@ buttonsSendMessage.forEach((buttonSend) =>
 // LOGIC: create project cards from javascript data
 
 // EVENT LISTENERS: automatically create project and carousel cards from array
-(userData.cardsProjects).forEach((card) => {
+(userInfo.cardsProjects).forEach((card) => {
   createProjectAndCarouselCard(
     { value: card.project },
     { value: card.technology }
@@ -770,7 +725,7 @@ buttonsSendMessage.forEach((buttonSend) =>
 });
 
 // EVENT LISTENERS: automatically create message from array
-(userData.messages).forEach((message) => {
+(userInfo.messages).forEach((message) => {
   createMessage(
     {value: message.name},
     {value: message.email},
@@ -848,7 +803,7 @@ function createTechSkill(skill, num) {
 }
 
 // EVENT LISTENERS: create skill container from array
-(userData.techSkill).forEach((tech) => {
+(userInfo.techSkill).forEach((tech) => {
   createTechSkill(tech.skill, tech.experience);
 });
 
---
+++ userInfo.js
@@ -0,0 +1,50 @@
+// ELEMENTS: main user data object
+
+const userInfo = {
+  firstName: "Krzysztof",
+  lastName: "Włodowski",
+  info: {
+    introduce:
+      "Hello! I'm Krzysztof Włodowski, a person passionate about continuous growth and exploring new paths.\n My background is diverse, my approach to life is rooted in acquiring knowledge and achieving goals with full dedication.\n Feel free to discover more about my professional journey and interests!",
+    myBackground:
+      "I have a diverse background that enriches both my professional career and personal growth.\n I am constantly striving to develop myself and make progressin every area of life I am involved in.\n My professional experience has taught me to be diligent incarrying out my responsibilities, take ownership of assignedtasks, and aim for the best possible final result.",
+    hobbies:
+      "In my free time, I enjoy developing my passions, especially physical activity and DIY projects.\n Both of these hobbies allow me to spend my time creatively and productively, while constantly growing and becoming better at what I do.",
+  },
+  techSkill: [
+    { skill: "HTML", experience: 2 },
+    { skill: "CSS", experience: 1 },
+    { skill: "Java Script", experience: 1 },
+    { skill: "Git", experience: 1 },
+    { skill: "Figma", experience: 1 },
+    { skill: "Chrome", experience: 4 },
+    { skill: "VSCode", experience: 2 },
+    { skill: "GitHub", experience: 1 },
+  ],
+  cardsProjects: [
+    { project: "Calculator", technology: "HTML" },
+    { project: "Non-governmental organization", technology: "HTML,CSS" },
+    { project: "Calculator program", technology: "Java Script" },
+    { project: "Calculator", technology: "HTML,C++" },
+    { project: "Non-governmental organization", technology: "HTML,CSS" },
+  ],
+  messages: [
+    {
+      name: "Karol",
+      email: "karol@email.com",
+      message: "Hello, I've reviewed your impressive portfolio and am interested in disscussing a potential collaboration. Please call me at 712-218-123 to talk further."
+    },
+    {
+      name: "Ernest",
+      email: "ernest@email.com",
+      message: "Hello, Please call me at 351-152-555 to talk further."
+    },
+    {
+      name: "Jan",
+      email: "jan@email.com",
+      message: "Welcome Jan. You created really nice project"
+    }
+  ]
+};
+
+export default userInfo
