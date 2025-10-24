851a8c6acdbd93c8b9d29b2c9d81c79d91de780a
Kris1990_PL <MRK1990@wp.pl>
2025-10-24 21:28:15 +0200

    refactor(structureApp): extend structure

--- structureITPortfolio.js
+++ structureITPortfolio.js
@@ -23,6 +23,33 @@ const structureApp = {
       paragraph: "MESSAGES FROM THE\n INTERESTED PERSON",
     },
   },
+  formInfo: {
+    label: {
+      name: "Name",
+      email: "Email",
+      message: "Message",
+      project: "Project title",
+      technology: "Technologies",
+    },
+    inputPlaceholder: {
+      name: "Your name",
+      email: "email@example.com",
+      message: "Hello, my name is...",
+      project: "Project title",
+      technology: "html,css,javascript",
+    },
+    errorMessage: {
+      min: function minErrorMessage(word, digit) {
+        return `The ${word} must be at least ${digit} characters long`;
+      },
+      max: function maxErrorMessage(word, digit) {
+        return `The ${word} must not exceed ${digit} characters.`;
+      },
+      email: "Please enter a valid email address",
+      message: "The message cannot be empty",
+      technology: "Please add some technologies"
+    },
+  },
 };
 
 export default structureApp;
